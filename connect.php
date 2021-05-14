813fb0734b10f722b2a21a4bec5d2e9b153de181
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-05-14 16:55:49 +0500

    ,

--- connect.php
+++ connect.php
@@ -26,36 +26,3 @@ $loginresults     = $unifi_connection->login();
 $auth_result = $unifi_connection->authorize_guest($mac, $duration, null, null, null, $ap);
 
 header("Location: thankyou.htm");
-
-?>
-
-<!DOCTYPE HTML>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>HKHC</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  <link rel="stylesheet" href="bulma.min.css" />
-  <link rel="icon" type="image/png" href="favicomatic\favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="favicomatic\favicon-16x16.png" sizes="16x16" />
-  <link rel="stylesheet" href="style.css" />  
-</head>
-<body>
-	<div class="bg">
-
-    <figure id="logo">
-        <img src="logo.png">
-    </figure>
-
-    <br>
-    <br>
-
-		<div id="handle" class="content is-size-6">Please wait, you are being </div>
-		<div id="devices" class="content is-size-6">authorized on WiFi</div>
-
-    <div id="powered_connect" class="content is-size-6">HKHC</div>
-    <div id="copyright" class="content is-size-6">(C) Copyright 2021</div>
-
-  </div>
-</body>
-</html>
